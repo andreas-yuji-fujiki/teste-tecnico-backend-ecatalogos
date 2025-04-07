d3a181b14424c9f030df43afa1fc2848d013c6fd
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-04-06 21:35:50 -0300

    reset projet to start from scratch

--- src/middlewares/globalErrorMiddlewares.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const globalErrorMiddleware = ( err: Error, req: Request, res: Response, next: NextFunction ) => {
-    console.log(err);
-    res.status(500).json({ message: "Internal server error! "})
-}
