b9d252e58b6c782247ca27a1f6f3be49fb5c0451
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-04-07 02:03:38 -0300

    fix get all products middleware

--- src/middlewares/product/getAllProducts.middleware.ts
+++ src/middlewares/product/getAllProducts.middleware.ts
@@ -4,7 +4,7 @@ export const getAllProductsMiddleware: RequestHandler = (req, res, next) => {
   const { name } = req.query;
 
   // if there is not a "name", or "name" is not a string
-  if (!name || typeof name !== "string") {
+  if (name && typeof name !== "string") {
     res.status(400).json({
       status: 400,
       error: "Filtro inválido!",
